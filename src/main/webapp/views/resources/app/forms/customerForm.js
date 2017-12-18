325b59f8100c13648567f7878b33a2a8eeefd0a8
Konstantin Fedotovskih <ksfprivate@gmail.com>
2017-12-18 22:12:33 +0500

    Material Design

--- src/main/webapp/views/resources/app/forms/customerForm.js
+++ src/main/webapp/views/resources/app/forms/customerForm.js
@@ -1,8 +1,23 @@
 var CustomerForm  = {
 
-    create: function () {
+    setData: function () {
 
-       this.form = DynamicForm.create({
+    },
+
+    cardExpand: function () {
+        console.log("dfd");
+    },
+
+    create: function () {
+         var header = HTMLFlow.create({
+            contents:
+            "<table class='cardBoxTitle'><tr>"+
+            "<td><input id='cardBoxExpandButton' type='image' src='"+imgDir+"/ic_expand.png' class='img' onclick=cardExpand()></td>"+
+            "<td width='100%'>Организация</td>"+
+            "</tr></table>"
+        });
+
+       this.titleBlock = DynamicForm.create({
            width: "100%",
            numCols: 2,
            colWidths: [100, "*"],
@@ -16,32 +31,43 @@ var CustomerForm  = {
             ]
         });
 
-       this.title = HTMLFlow.create({
-           contents: "<div class='cardBoxTitle'>Организация</div>"
-       });
+       this.addressBlock = DynamicForm.create({
+            titleOrientation : "top",
+            width: 500,
+            numCols: 3,
+            padding:6,
+            margin: 8,
+            autoDraw:false,
+            fields: [
+                {name: "postIndex", title: "Индекс", type: "text"},
+                {name: "district", title: "Регион", type: "text"},
+                {name: "region", title: "Область", type: "text"},
+                {name: "city", title: "Город", type: "text"},
+                {name: "address", title: "Улица", type: "text"},
+                {name: "building", title: "Дом", type: "text"}
+            ]
+        });
 
-        this.sectionTitle =
+
+       function blockTitle(title) {
+           return HTMLFlow.create({
+               contents: "<div class='cardBoxSectionTitle'>"+title+"</div><div class='cardBoxSeparator'/>"
+           });}
 
         this.content = VLayout.create({
             width: "100%",
+            height: "300",
             autoDraw: false,
             members: [
-                this.title,
-                HTMLFlow.create({
-                    contents: "<div class='cardBoxSectionTitle'>Наименование</div><div class='cardBoxSeparator'/>"
-                }),
-                this.form,
-                HTMLFlow.create({
-                    contents: "<div class='cardBoxSectionTitle'>Адрес</div><div class='cardBoxSeparator'/>"
-                })
+                header,
+                blockTitle("Наименование"),
+                this.titleBlock,
+                blockTitle("Адрес"),
+                this.addressBlock
             ]
         });
 
        this.content.setStyleName("cardBox");
        return Object.create(this);
-    },
-
-    setData: function () {
-
     }
 };
--- src/main/webapp/views/resources/app/frames/browserFrame.js
+++ src/main/webapp/views/resources/app/frames/browserFrame.js
@@ -8,10 +8,12 @@ function createBrowserFrame() {
     return(
         VLayout.create({
         width: "100%",
-        height: "*",
+        height: "100%",
         padding:8,
         margin: 8,
-        backgroundColor: "#EEEEEE",
+        border:0,
+        overflow:"auto",
+            styleName: "browserArea",
         members: [
            customerForm.content, separator,
            customerForm.create().content
