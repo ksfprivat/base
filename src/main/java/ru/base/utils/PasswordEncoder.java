e81db2d6a262faa23e82f3fa234c767d9b0c00f6
Konstantin Fedotovskih <ksfprivate@gmail.com>
2018-02-14 17:24:31 +0500

    Spring users

--- src/main/java/ru/base/utils/PasswordEncoder.java
+++ src/main/java/ru/base/utils/PasswordEncoder.java
@@ -5,8 +5,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class PasswordEncoder {
     public static void main(String[] args) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
-        System.out.println(encoder.encode("admin"));
-        System.out.println(encoder.encode("user"));
-
+        System.out.println(encoder.encode("d1857s"));
+        System.out.println(encoder.encode("m8639r"));
+        System.out.println(encoder.encode("s4021q"));
     }
 }
