1bcd383930963e0edf4692b8ce82dd14ac7a2d75
Konstantin Fedotovskih <ksfprivate@gmail.com>
2018-05-04 17:31:02 +0500

    New Contracts window - final

--- src/main/webapp/views/resources/app/frames/navContractsGrid.js
+++ src/main/webapp/views/resources/app/frames/navContractsGrid.js
@@ -1,13 +1,27 @@
 NavContractsGrid = {
     create: function () {
+        this.currentRecord ={};
+        this.dataSource = DataSource.create({
+            fields: [
+                {name: "id", primaryKey: true, hidden: true},
+                {name: "title"},
+                {name: "date", hidden:true},
+                {name: "year", hidden:true} ,
+                {name: "amount", hidden:true},
+                {name: "status", hidden: true},
+                {name: "dateFinal", hidden:true},
+                {name: "customerTitle", hidden:true}
+            ],
+            clientOnly: true
+        });
 
         this.listGrid = ListGrid.create({
             width: "100%",
             height: "100%",
             border:0,
-            cellHeight: 44,
-            alternateRecordStyles: true,
-            alternateFieldStyles: true,
+            cellHeight: 76,
+            // alternateRecordStyles: true,
+            // alternateFieldStyles: true,
             showHeader: false,
             autoDraw: false,
             sortField: "date",
@@ -15,6 +29,7 @@ NavContractsGrid = {
             virtualScrolling: false,
             autoFetchData:true,
             dataPageSize: 10000,
+            baseStyle: 'cellContractRow',
             fields: [
                 {name: "id", hidden: true },
                 {name: "title"},
@@ -22,9 +37,25 @@ NavContractsGrid = {
                 {name: "year"},
                 {name: "amount", hidden: true},
                 {name: "dateFormat",hidden: true},
+                {name: "status", hidden: true},
+                {name: "dateFinal", hidden:true},
                 {name: "customerTitle", hidden:true}
             ],
             rowClick: this.onRowClick
+            // getBaseStyle:function (record, rowNum, colNum) {
+            //
+            //
+            //     if ((record.status === "Исполнение") || (record.status === "1"))  {
+            //         // if ((new Date()) > (new Date(record.dateFinal)))
+            //         //     return "cellRed";
+            //         // else
+            //          return "cellLightGreen"
+            //     }
+            //     else
+            //     if (record.status === "0") return "cellLightYellow";
+            //     else
+            //         return this.baseStyle;
+            // }
         });
 
         this.init();
@@ -34,51 +65,79 @@ NavContractsGrid = {
 
     init: function () {
 
-        getAllContracts(function (contracts, success) {
-            var dataSource = DataSource.create({
-                fields: [
-                    {name: "id", primaryKey: true, hidden: true},
-                    {name: "title"},
-                    {name: "date", hidden:true},
-                    {name: "year", hidden:true} ,
-                    {name: "amount", hidden:true},
-                    {name: "customerTitle", hidden:true}
-                ],
-                clientOnly: true
-            });
-            // dataSource.setCacheData(contracts);
-
+        getAllContracts(function (contracts) {
+            var title = "";
             for (var i = 0; i < contracts.length; i++) {
-                var title = NavContractsGrid.createItemBlock(contracts[i]);
-                dataSource.addData({id: contracts[i].id, title: title,
-                    date: contracts[i].date, year: contracts[i].year, amount: contracts[i].amount,
+                title = NavContractsGrid.createItemBlock(contracts[i]);
+                NavContractsGrid.dataSource.addData({id: contracts[i].id, title: title,
+                    date: contracts[i].date, year: contracts[i].year, status: contracts[i].status,
                     customerTitle:contracts[i].customerTitle});
             }
-            NavContractsGrid.listGrid.setDataSource(dataSource);
+            NavContractsGrid.listGrid.setDataSource(NavContractsGrid.dataSource);
         });
     },
 
-    onRowClick: function (record, recordNum, fieldNum) {
-        // console.log(record);
-
+    onRowClick: function (record) {
+        NavContractsGrid.currentRecord = record;
     },
 
+
     createItemBlock: function(contract) {
-        return (
+        var bgColor = "#757575";
+        var keyWord = "Недействителен";
+        switch  (contract.status) {
+            case "Исполнение":
+            case "1":
+                bgColor = "#64DD17";
+                keyWord = "Исполение";
+                break;
+            case "Подписание":
+            case "0":
+                bgColor = "#FF8F00";
+                keyWord = "Подписание";
+                break;
+            case "Выполнено":
+                bgColor = "#0288D1";
+                keyWord = "Выполнено";
+                break;
+        }
+
+        var block = (
+            "<div>"+
             "<table class='listItem'><tr>"+
-                 "<td><img src='"+imgDir+"/ic_currency.png'></td>"+
+                 "<td width='22px'><img src='"+imgDir+"/ic_currency.png' width='22px'></td>"+
                  "<td >"+contract.title+"</td>" +
-                 "<td align='right' style='color: #009688' >"+formatStringDoubleToCurrency(contract.amount)+"&nbsp;₽</td>" +
+                 "<td align='right' style='color: #00b956'>"+formatStringDoubleToCurrency(contract.amount)+"&nbsp;₽</td>" +
             "</tr></table>" +
-            "<table width='100%'><tr>" +
+            "<table width='100%' style='border-bottom: 1px; border-color: #9e9e9e'><tr>" +
                  "<td align='left'><small style='color: #0D47A1'>"+contract.customerTitle+"<small/></td>" +
                  "<td align='right'><small style='color: #2962FF'>"+formatDateString(contract.date)+"<small/></td>"+
-            "</tr></table>"
+            "</tr>" +
+            "<tr ><td>" +
+                "<a id ='statusButton' href='#' style='text-decoration: none' onclick='NavContractsGrid.statusButtonClick()'><small " +
+                    "style='color: white; " +
+                    "font-family: RobotoLight; " +
+                    "background-color:"+ bgColor+";"+
+                    "padding: 2px; " +
+                    "border-radius: 3px'>" +
+                    "&nbsp;"+keyWord+"&nbsp;" +
+                "</small></a>" +
+            "</td></tr></table>"+
+            "</div>"
         );
+
+        return block;
     },
 
     setFilter: function (filter) {
-        this.listGrid.filterData([filter]);
+       NavContractsGrid.listGrid.filterData(filter);
+
+    },
+
+    statusButtonClick: function (record) {
+        // NavContractsGrid.listGrid.getRecord(NavContractsGrid.listGrid.getFocusRow());
+        console.log(NavContractsGrid.listGrid.getFocusRow());
+
     },
 
     clearFilter: function () {
